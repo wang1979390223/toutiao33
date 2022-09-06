c45448a80154b9d25e5e2576d6a1badb39eb11a3
王三一 <1979390223@qq.com>
2022-09-06 18:12:50 +0800

    feat:登陆页完成布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+//  import Login from '@/views/Login'
+//  首屏加载速度很慢，可以使用路由懒加载，只有使用到了再去请求
 Vue.use(VueRouter)
 
 const routes = [
-
+  {
+    path: '/login',
+    //  component: Login，下面路由懒加载
+    component: () => import('@/views/Login')
+  }
 ]
 
 const router = new VueRouter({
