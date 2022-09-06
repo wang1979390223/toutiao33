569b1c1f5bab9bb7b5187f8b50b8d35c84cf9964
王三一 <1979390223@qq.com>
2022-09-06 16:13:19 +0800

    feat:postcss配置完毕

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div>
+    <span class="toutiao toutiao-dianzan"></span>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    黑马头条
+    <div class="box" >登录</div>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+//  1，git仓库管理
+//  git log 查看commit记录
+//  git status 查看当前仓库的状态
+//  git add .  提交到暂存区
+//  git commit
+//  2，引入vant组件
+//  3，rem适配  下包yarn add amfe-flexible
+//  自动把css中的px转成rem post-css css的预处理器
+//  下包yarn add postcss-pxtorem
+//  在根目录下创建   .postcssrc.js
+
+export default {}
+</script>
+
+<style scoped>
+.box {
+  width:750px;
+  height: 90px;
+background-color:skyblue;
+font-size: 40px;
 }
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
